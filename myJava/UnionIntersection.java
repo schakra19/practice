a67f04bf83044467f7fd6a1512ba5394cf61c0ab
Sukanya Chakraborty <sukanya@cs.wisc.edu>
2016-02-24 00:40:02 -0600

    Update UnionIntersection.java

--- myJava/UnionIntersection.java
+++ myJava/UnionIntersection.java
@@ -1,5 +1,5 @@
-/* Given two arrays, find the union and intersection 
-of elements of both arrays */
+/* Given two sorted arrays, find the union and 
+intersection of elements of both arrays */
 public class UnionIntersection{
 
 	static void unionSorted(int a[], int b[]){
