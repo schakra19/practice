49e61de570d7989887c6a778ef642b713e6fe502
Sukanya Chakraborty <sukanya@cs.wisc.edu>
2016-04-10 13:53:02 -0500

    Update PrintAsci.java
    Java program to print asci values of all alphabets

--- PrintAsci.java
+++ PrintAsci.java
@@ -2,13 +2,13 @@
 public class PrintAsci {
 
         public static void main(String[] args){
-                char a = '\0';
+                char null_char = '\0';
                 for(char c='a'; c<='z';c++){
-                        int value = c - a;
+                        int value = c - null_char;
                         System.out.println(c + " == "+ value);
                 }
                 for(char c='A'; c<='Z'; c++){
-                        int value = c -a;
+                        int value = c -null_char;
                         System.out.println(c + " == " + value);
                 }
         }
